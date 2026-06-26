c073d71bab05ff45470232fc5279a12c1222f292
tybnasgo <tybnasgo@naver.com>
2026-06-26 23:59:19 +0900

    몬스터 크기 조정

--- Assets/EverPlanet/Scripts/Player/PlayerInfo.cs
+++ Assets/EverPlanet/Scripts/Player/PlayerInfo.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerInfo : MonoBehaviour
 {
+    //이동 관련
+    public float playerSpeed;
+    public float playerJumpPower;
+    public int maxJumpCount;
     
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
