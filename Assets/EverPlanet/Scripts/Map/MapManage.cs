7778748302481ef160c4c90a0eefa5af75488d46
tybnasgo <tybnasgo@naver.com>
2026-07-18 12:39:13 +0900

    포탈 배치 및 로직 작성

---
+++ Assets/EverPlanet/Scripts/Map/MapManage.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManage : MonoBehaviour
+{
+    public List<Portal> portalList = new();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/EverPlanet/Scripts/Map/Portal.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    //맵 관리
+    public MapManage mapManage;
+    //포탈 번호
+    public int portalNum;
+    public int nextNum;
+    //플레이어 태그
+    public string playerString = "Player";
+
+    //플레이어
+    public GameObject playerObj;
+
+    private void Awake()
+    {
+        playerObj = GameObject.Find("Player");
+        mapManage = GameObject.Find("Map").GetComponent<MapManage>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(playerString))
+        {
+            playerObj.gameObject.transform.position = mapManage.portalList[nextNum].transform.position+0.5f*Vector3.up;
+        }
+    }
+}
